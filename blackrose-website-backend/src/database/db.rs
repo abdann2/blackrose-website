693da71b8d2a0558e418e9efc98449cbb846e7b7
abdann2 <abdann2009@outlook.com>
2023-04-20 23:11:09 -0500

    Added DbInterface Constructor, made DbInteface fields public

--- blackrose-website-backend/src/database/db.rs
+++ blackrose-website-backend/src/database/db.rs
@@ -1,8 +1,18 @@
+use diesel::ConnectionError;
+use diesel_async::AsyncConnection;
 use diesel_async::AsyncPgConnection;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-struct DbInterface {
+pub struct DbInterface {
     // Put it in an async arc mutex for thread safety
-    db: Arc<Mutex<AsyncPgConnection>>,
+    pub db: Arc<Mutex<AsyncPgConnection>>,
+}
+
+impl DbInterface {
+    pub async fn new(database_url: &str) -> Result<Self, ConnectionError> {
+        let conn = AsyncPgConnection::establish(database_url).await?;
+        let conn = Arc::new(Mutex::new(conn));
+        Ok(Self { db: conn })
+    }
 }
