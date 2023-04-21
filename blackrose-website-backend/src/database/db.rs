22fb83a0ec796a9fa41fc9c92d6a9b02fef91b6c
abdann2 <abdann2009@outlook.com>
2023-04-20 23:08:08 -0500

    Revert "Added DbInterface constructor"
    This reverts commit af14e465e256b9013eebcef8c36360e00084b1f4.

--- blackrose-website-backend/src/database/db.rs
+++ blackrose-website-backend/src/database/db.rs
@@ -1,17 +1,8 @@
-use diesel::ConnectionError;
-use diesel_async::AsyncConnection;
 use diesel_async::AsyncPgConnection;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+
 struct DbInterface {
     // Put it in an async arc mutex for thread safety
     db: Arc<Mutex<AsyncPgConnection>>,
-}
-
-impl DbInterface {
-    pub async fn new(database_url: &str) -> Result<Self, ConnectionError> {
-        let conn = AsyncPgConnection::establish(database_url).await?;
-        let conn = Arc::new(Mutex::new(conn));
-        Ok(Self { db: conn })
-    }
 }
