8ac54ca35619975f89e5a01213c0218a4490c6fc
abdann2 <abdann2009@outlook.com>
2023-05-03 13:12:48 -0500

    Revert "renamed column field for tokens"
    This reverts commit 01329d51f59c18d488fc5204db184e4a5f2f7903.

--- blackrose-website-backend/migrations/2023-05-03-054307_token_registration/up.sql
+++ blackrose-website-backend/migrations/2023-05-03-054307_token_registration/up.sql
@@ -2,8 +2,8 @@
 ALTER TABLE users
 ADD registration_confirmed bool NOT NULL;
 CREATE TABLE registration_tokens (
-    id int NOT NULL,
+    user_id int NOT NULL,
     registration_token text NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES users(id)
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
